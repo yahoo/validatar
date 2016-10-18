47ceff81bfc0554eed0c4bafe78447fececde2e4
Akshai Sarma <asarma@yahoo-inc.com>
2016-10-18 10:13:20 -0700

    Reducing copyright headers

--- src/test/java/com/yahoo/validatar/FakeTestClass.java
+++ src/test/java/com/yahoo/validatar/FakeTestClass.java
@@ -1,19 +1,7 @@
 /*
- * Copyright 2014-2016 Yahoo! Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2015 Yahoo Inc.
+ * Licensed under the terms of the Apache 2 license. Please see LICENSE file in the project root for terms.
  */
-
 package com.yahoo.validatar;
 
 import com.yahoo.validatar.common.Helpable;
